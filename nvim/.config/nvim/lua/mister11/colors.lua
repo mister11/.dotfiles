6b3a864db7654a15122707ef2ec8ff15110f5729
Sven Vidak <sven.vidak@gmail.com>
2023-04-23 00:05:49 +0200

    fix: fix nvim theme for Elixir

--- nvim/.config/nvim/lua/mister11/colors.lua
+++ nvim/.config/nvim/lua/mister11/colors.lua
@@ -1,2 +1,4 @@
 vim.g.gruvbox_baby_background_color = "dark"
+local colors = require("gruvbox-baby.colors").config()
+vim.g.gruvbox_baby_highlights = {["@symbol"] = {fg = colors.soft_yellow}}
 vim.cmd([[colorscheme gruvbox-baby]])
