10d4fb706741150f6704d7b371a4c4dc248da116
Sven Vidak <sven.vidak@gmail.com>
2024-03-01 03:17:45 +0100

    feat: rofi updates and new nvim plugins

--- nvim/.config/nvim/lua/mister11/lazy.lua
+++ nvim/.config/nvim/lua/mister11/lazy.lua
@@ -12,6 +12,33 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    -- new stuff
+    {
+        "j-hui/fidget.nvim",
+        opts = {}
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
+    {
+        "nvim-pack/nvim-spectre",
+        build = false,
+        cmd = "Spectre",
+        opts = { open_cmd = "noswapfile vnew" },
+        -- stylua: ignore
+        keys = {
+            { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+        },
+    },
     -- copilot
     { "github/copilot.vim" },
 
@@ -26,10 +53,10 @@ require("lazy").setup({
     -- project explorer
     {
         'nvim-tree/nvim-tree.lua',
+        version = "*",
         dependencies = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly'                    -- optional, updated every week. (see issue #1193)
+            "nvim-tree/nvim-web-devicons",
+        }
     },
 
     -- telescope
