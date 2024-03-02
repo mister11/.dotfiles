d31a91812bbe7f176cb239e22658008cf1db6029
Sven Vidak <sven.vidak@gmail.com>
2024-03-02 01:34:15 +0100

    feat: set specific tag for treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    tag = "v0.9.2",
     build = ":TSUpdate",
     config = function()
         local configs = require("nvim-treesitter.configs")
@@ -35,36 +36,6 @@ return {
             autotag = {
                 enable = true,
                 filetypes = filetypes,
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-
-                    -- Automatically jump forward to textobj, similar to targets.vim
-                    lookahead = true,
-
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                    },
-                    selection_modes = {
-                        ['@parameter.outer'] = 'v', -- charwise
-                        ['@function.outer'] = 'V', -- linewise
-                        ['@class.outer'] = '<c-v>', -- blockwise
-                    },
-                },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        -- ["<leader>a"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        -- ["<leader>A"] = "@parameter.inner",
-                    },
-                },
             }
         })
     end
