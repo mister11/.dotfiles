a0dd0241d29e7574609a8ada363dee2658a0b92d
Sven Vidak <sven.vidak@gmail.com>
2026-07-01 23:50:49 +0200

    feat: add workspace rules

---
+++ hypr/.config/hypr/config/workspaces.lua
@@ -0,0 +1,11 @@
+hl.workspace_rule({ workspace = "1", monitor = "desc:BOE NE135A1M-NY1", default = true })
+hl.workspace_rule({ workspace = "8", monitor = "desc:BOE NE135A1M-NY1"})
+hl.workspace_rule({ workspace = "9", monitor = "desc:BOE NE135A1M-NY1"})
+hl.workspace_rule({ workspace = "10", monitor = "desc:BOE NE135A1M-NY1" })
+
+hl.workspace_rule({ workspace = "2", monitor = "desc:Dell Inc. DELL G2724D 9WG06Y3"})
+hl.workspace_rule({ workspace = "3", monitor = "desc:Dell Inc. DELL G2724D 9WG06Y3", default = true })
+hl.workspace_rule({ workspace = "4", monitor = "desc:Dell Inc. DELL G2724D 9WG06Y3", default = true })
+hl.workspace_rule({ workspace = "5", monitor = "desc:Dell Inc. DELL G2724D 9WG06Y3", default = true })
+hl.workspace_rule({ workspace = "6", monitor = "desc:Dell Inc. DELL G2724D 9WG06Y3", default = true })
+hl.workspace_rule({ workspace = "7", monitor = "desc:Dell Inc. DELL G2724D 9WG06Y3", default = true })
